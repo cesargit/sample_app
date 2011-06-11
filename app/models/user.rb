22cdebf39ad2a7a9ff004765458422e55b7a15f3
Cesar Rodriguez <cesar_j_rodriguez@hotmail.com>
2011-06-11 08:27:51 -0700

    Added password and encrypted_password attributes to the User model_rev

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@
 #
 # Table name: users
 #
-#  id                :integer         not null, primary key
-#  name              :string(255)
-#  email             :string(255)
-#  created_at        :datetime
-#  updated_at        :datetime
-#  encipted_password :string(255)
+#  id                 :integer         primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
 class User < ActiveRecord::Base  
--- db/schema.rb
+++ db/schema.rb
@@ -17,9 +17,7 @@ ActiveRecord::Schema.define(:version => 20110610171451) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "encipted_password"
+    t.string   "encrypted_password"
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
